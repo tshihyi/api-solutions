ee78cce6abc555358e93240e7c0dfb11792b1e29
shelly <tshihyi@gmail.com>
2022-09-11 13:49:09 +0800

    Transform uppercase to lowercase

--- customer-point-transaction-log.py
+++ customer-point-transaction-log.py
@@ -20,110 +20,98 @@ def read_root():
     return data
 
 parameter_validate = {
-    'Account_Customer_No':
+    'account_customer_no':
     Query(default=None,
           description="身份證字號-生日, 護照號碼-生日",
           min_length=10,
           max_length=30),
-    'As_Of_Date_Start':
+    'as_of_date_start':
     Query(default=None, description="資料開始日, YYYY-MM-DD"),
-    'As_Of_Date_End':
+    'as_of_date_end':
     Query(default=None, description="資料結束日, YYYY-MM-DD"),
-    'Atm_id':
-    Query(default=None, description="ATM編號", max_length=8),
-    'Bank_Nbr':
+    'bank_no':
     Query(default=None, description="分行代碼", max_length=5),
-    'Transaction_DateTime_Start':
+    'transaction_datetime_start':
     Query(default=None, description="點數交易開始日, YYYY-MM-DD HH:MM:SS"),
-    'Transaction_DateTime_End':
+    'transaction_datetime_end':
     Query(default=None, description="點數交易結束日, YYYY-MM-DD HH:MM:SS"),
-    'Project_Group_Id':
+    'project_group_id':
     Query(default=None, description="活動代碼", max_length=80),
-    'Source_Table':
-    Query(
-        default=2,
-        description="來源資料表：點數中台資料表",
-        enum=["1", "2", "3"],
-    ),
-    'Transaction_Memo':
+    'transaction_memo':
     Query(default=None, description="交易註記", max_length=80),
-    'Transaction_Nbr':
+    'transaction_no':
     Query(default=None, description="交易編號", max_length=10),
-    'Source_Channel':
+    'source_channel':
     Query(
         default=None,
         description="H:行動APP, E:網銀, L: Line, S: 系統, O: 其他",
         enum=["H", "E", "L", "S", "O"],
     ),
-    'Transaction_Type':
+    'transaction_type':
     Query(default=None, description="交易類型，HEG:兌換商品, LOTTERY:抽獎", max_length=3)
 }
 
 
 @app.get("/items/")
 async def read_item(
-    Account_Customer_No: Union[
-        str, None] = parameter_validate["Account_Customer_No"],
-    As_Of_Date_Start: Union[date, None] = parameter_validate["As_Of_Date_Start"],
-    As_Of_Date_End: Union[date, None] = parameter_validate["As_Of_Date_End"],
-    Bank_Nbr: Union[str, None] = parameter_validate["Bank_Nbr"],
-    Transaction_DateTime_Start: Union[
-        datetime, None] = parameter_validate["Transaction_DateTime_Start"],
-    Transaction_DateTime_End: Union[
-        datetime, None] = parameter_validate["Transaction_DateTime_End"],
-    Project_Group_Id: Union[str,
-                            None] = parameter_validate["Project_Group_Id"],
-    Source_Table: Union[str, None] = parameter_validate["Source_Table"],
-    Transaction_Memo: Union[str,
-                            None] = parameter_validate["Transaction_Memo"],
-    Transaction_Nbr: Union[str, None] = parameter_validate["Transaction_Nbr"],
-    Source_Channel: Union[str, None] = parameter_validate["Source_Channel"],
-    Transaction_Type: Union[str,
-                            None] = parameter_validate["Transaction_Type"]):
+    account_customer_no: Union[
+        str, None] = parameter_validate["account_customer_no"],
+    as_of_date_start: Union[date, None] = parameter_validate["as_of_date_start"],
+    as_of_date_end: Union[date, None] = parameter_validate["as_of_date_end"],
+    bank_no: Union[str, None] = parameter_validate["bank_no"],
+    transaction_datetime_start: Union[
+        datetime, None] = parameter_validate["transaction_datetime_start"],
+    transaction_datetime_end: Union[
+        datetime, None] = parameter_validate["transaction_datetime_end"],
+    project_group_id: Union[str,
+                            None] = parameter_validate["project_group_id"],
+    transaction_memo: Union[str,
+                            None] = parameter_validate["transaction_memo"],
+    transaction_no: Union[str, None] = parameter_validate["transaction_no"],
+    source_channel: Union[str, None] = parameter_validate["source_channel"],
+    transaction_type: Union[str,
+                            None] = parameter_validate["transaction_type"]):
 
     query_list = ['SELECT * FROM CUST_POINT_TXN WHERE 1 = 1 ']
 
     item = {}
 
     #query parameters
-    if Account_Customer_No:
-        item.update({"Account_Customer_No": Account_Customer_No + "%"})
+    if account_customer_no:
+        item.update({"account_customer_no": account_customer_no + "%"})
         query_list.append(
-            'AND `Account_Customer_No` like :Account_Customer_No ')
-    if As_Of_Date_Start:
-        item.update({"As_Of_Date_Start": As_Of_Date_Start})
-        query_list.append('AND `As_Of_Date` BETWEEN CAST(DATE(:As_Of_Date_Start) as TEXT)')
-    if As_Of_Date_End:
-        item.update({"As_Of_Date_End": As_Of_Date_End})
-        query_list.append('AND CAST(DATE(:As_Of_Date_End) as TEXT) ')
-    if Bank_Nbr:
-        item.update({"Bank_Nbr": Bank_Nbr})
-        query_list.append('AND `Bank_Nbr` = :Bank_Nbr ')
-    if Transaction_DateTime_Start:
-        item.update({"Transaction_DateTime_Start": Transaction_DateTime_Start})
-        query_list.append('AND `Modify_DateTime` BETWEEN CAST(DATETIME(:Transaction_DateTime_Start) as TEXT) ')
-    if Transaction_DateTime_End:
-        item.update({"Transaction_DateTime_End": Transaction_DateTime_End })
-        query_list.append('AND CAST(DATETIME(:Transaction_DateTime_End) as TEXT) ')
-    if Project_Group_Id:
-        item.update({"Project_Group_Id": Project_Group_Id + "%"})
-        query_list.append('AND `Project_Group_Id` like :Project_Group_Id ')
-    if Source_Table:
-        item.update({"Source_Table": Source_Table})
-        query_list.append('AND `Source_Table_Code` = :Source_Table ')
-    if Transaction_Memo:
-        item.update({"Transaction_Memo": Transaction_Memo + "%"})
-        query_list.append('AND `Txn_Memo` like :Transaction_Memo ')
-    if Transaction_Nbr:
-        item.update({"Transaction_Nbr": Transaction_Nbr + "%"})
-        query_list.append('AND `Txn_Nbr` like :Transaction_Nbr ')
-    if Source_Channel:
-        item.update({"Source_Channel": Source_Channel + "%"})
+            'AND `account_customer_no` like :account_customer_no ')
+    if as_of_date_start:
+        item.update({"as_of_date_start": as_of_date_start})
+        query_list.append('AND `as_of_date` BETWEEN CAST(DATE(:as_of_date_start) as TEXT)')
+    if as_of_date_end:
+        item.update({"as_of_date_end": as_of_date_end})
+        query_list.append('AND CAST(DATE(:as_of_date_end) as TEXT) ')
+    if bank_no:
+        item.update({"bank_no": bank_no})
+        query_list.append('AND `bank_no` = :bank_no ')
+    if transaction_datetime_start:
+        item.update({"transaction_datetime_start": transaction_datetime_start})
+        query_list.append('AND `Modify_DateTime` BETWEEN CAST(DATETIME(:transaction_datetime_start) as TEXT) ')
+    if transaction_datetime_end:
+        item.update({"transaction_datetime_end": transaction_datetime_end })
+        query_list.append('AND CAST(DATETIME(:transaction_datetime_end) as TEXT) ')
+    if project_group_id:
+        item.update({"project_group_id": project_group_id + "%"})
+        query_list.append('AND `project_group_id` like :project_group_id ')
+    if transaction_memo:
+        item.update({"transaction_memo": transaction_memo + "%"})
+        query_list.append('AND `txn_memo` like :transaction_memo ')
+    if transaction_no:
+        item.update({"transaction_no": transaction_no + "%"})
+        query_list.append('AND `txn_nbr` like :transaction_no ')
+    if source_channel:
+        item.update({"source_channel": source_channel + "%"})
         query_list.append(
-            'AND `Txn_Source_Channel_Code` like :Source_Channel ')
-    if Transaction_Type:
-        item.update({"Transaction_Type": Transaction_Type + "%"})
-        query_list.append('AND `Txn_Type_Code` like :Transaction_Type ')
+            'AND `txn_source_channel_code` like :source_channel ')
+    if transaction_type:
+        item.update({"transaction_type": transaction_type + "%"})
+        query_list.append('AND `txn_type_code` like :transaction_type ')
 
     sql_str = ' '.join(query_list)
 
@@ -137,23 +125,23 @@ async def read_item(
     return list(
         map(
             lambda item: {
-                "Account_Customer_No": item[0],
-                "As_Of_Date": item[1],
-                "Atm_Id": item[2],
-                "Bank_Nbr": item[3],
-                "Mission_Point": item[4],
-                "Modify_Date": item[5],
-                "Modify_DateTime": item[6],
-                "Project_Group_Id": item[7],
-                "Source_Table_Code": item[8],
-                "Total_Point": item[9],
-                "Total_Txn_Cnt": item[10],
-                "Txn_Cnt": item[11],
-                "Txn_Memo": item[12],
-                "Txn_Nbr": item[13],
-                "Txn_Point": item[14],
-                "Txn_Source_Channel_Code": item[15],
-                "Txn_Type_Code": item[16]
+                "account_customer_no": item[0],
+                "as_of_date": item[1],
+                "atm_id": item[2],
+                "bank_no": item[3],
+                "mission_point": item[4],
+                "modify_date": item[5],
+                "modify_datetime": item[6],
+                "project_group_id": item[7],
+                "source_table_code": item[8],
+                "total_point": item[9],
+                "total_txn_cnt": item[10],
+                "txn_cnt": item[11],
+                "txn_memo": item[12],
+                "txn_nbr": item[13],
+                "txn_point": item[14],
+                "txn_source_channel_code": item[15],
+                "txn_type_code": item[16]
             }, data))
 
 uvicorn.run(app, host="0.0.0.0", port=8080)
